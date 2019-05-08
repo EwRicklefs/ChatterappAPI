b7f8f217a029bf0b0cb97be69068e6f0048905da
EwRicklefs <ew.ricklefs@gmail.com>
2019-05-08 13:56:48 -0700

    testing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -63,6 +63,8 @@ module.exports = function(app) {
 
   app.post("/message/:room", (req, res) => {
     let idVal = 0
+    console.log(req.body.message)
+    console.log(req.body.userName)
     db.User.findOne({userName:req.body.userName}).then(user=> {
         console.log(user._id)
         idVal = user._id
